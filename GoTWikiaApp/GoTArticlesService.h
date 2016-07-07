b28553c1d06d0eb75bcf48966e8ce57e86727cab
magiczek91 <dwdmarkowski@gmail.com>
2016-07-07 23:18:52 +0200

    added Articles service

---
+++ GoTWikiaApp/GoTArticlesService.h
@@ -0,0 +1,11 @@
+//
+//  GoTArticlesService.h
+//  GoTWikia
+//
+//  Created by Dawid Markowski on 07.07.2016.
+//  Copyright © 2016 Dawid Markowski. All rights reserved.
+//
+
+@interface GoTArticlesService : NSObject
+
+@end
---
+++ GoTWikiaApp/GoTArticlesService.m
@@ -0,0 +1,19 @@
+//
+//  GoTArticlesService.m
+//  GoTWikia
+//
+//  Created by Dawid Markowski on 07.07.2016.
+//  Copyright © 2016 Dawid Markowski. All rights reserved.
+//
+
+@import Foundation;
+
+#import "GoTArticlesService.h"
+
+@interface GoTArticlesService ()
+
+@end
+
+@implementation GoTArticlesService
+
+@end
