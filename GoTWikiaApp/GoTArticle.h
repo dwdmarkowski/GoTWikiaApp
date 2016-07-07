9e1ee5a0fc29d94d85b09c3c04914b394e174b23
magiczek91 <dwdmarkowski@gmail.com>
2016-07-07 23:16:29 +0200

    Article model

--- GoTWikiaApp/GoTArticle.h
+++ GoTWikiaApp/GoTArticle.h
@@ -1,14 +1,11 @@
 //
-//  ViewController.h
-//  GoTWikiaApp
+//  GoTArticle.h
+//  GoTWikia
 //
 //  Created by Dawid Markowski on 07.07.2016.
 //  Copyright © 2016 Dawid Markowski. All rights reserved.
 //
 
-@import UIKit;
-
-@interface GoTArticlesViewController : UIViewController
+@interface GoTArticle : NSObject
 
 @end
-
---
+++ GoTWikiaApp/GoTArticle.m
@@ -0,0 +1,22 @@
+//
+//  GoTArticle.m
+//  GoTWikia
+//
+//  Created by Dawid Markowski on 07.07.2016.
+//  Copyright © 2016 Dawid Markowski. All rights reserved.
+//
+
+@import Foundation;
+
+#import "GoTArticle.h"
+
+@interface GoTArticle()
+
+@property (copy, nonatomic) NSString *title;
+@property (copy, nonatomic) NSString *abstract;
+
+@end
+
+@implementation GoTArticle
+
+@end
